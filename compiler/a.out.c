722a252c93b16969c04c95ab6d724982d79abd10
dodo <dbousque@student.42.fr>
2016-04-30 00:28:15 +0200

    worked a bit on code generation

---
+++ compiler/a.out.c
@@ -0,0 +1,20 @@
+
+
+#include "../language/language.h"
+
+int	__second_func(int lol)
+{
+int	__first_function(int a, int i)
+{
+void	__main(t_list* args)
+{
+
+
+int		main(int argc, char **argv)
+{
+	t_list	*args;
+
+	args = _args_to_list(argc, argv);
+	__main(args);
+	return (0);
+}
--- compiler/tree_to_c_code.c
+++ compiler/tree_to_c_code.c
@@ -18,11 +18,77 @@ void	write_end(int fd)
 	write(fd, str, ft_strlen(str));
 }
 
-void	write_function(int fd, t_linked_list *function)
+char	*get_type_str(int type)
+{
+	if (type == 0)
+		return (ft_strdup("void"));
+	else if (type == INTEGER)
+		return (ft_strdup("int"));
+	else if (type == STRING)
+		return (ft_strdup("t_string*"));
+	else if (type == CHAR)
+		return (ft_strdup("char"));
+	else if (type == DOUBLE)
+		return (ft_strdup("double"));
+	else if (type == FLOATING)
+		return (ft_strdup("float"));
+	else if (type == LONG)
+		return (ft_strdup("long"));
+	else if (type >= LIST)
+		return (ft_strdup("t_list*"));
+	return (NULL);
+}
+
+void	write_function_prototype(int fd, t_linked_list *function)
 {
 	t_node	*prototype;
+	char	*tmp_str;
+	t_node	*tmp_node;
 
-	prototype = tmp_node = ((t_node*)((t_sub_elt*)function->elts[0])->elt);
+	prototype = ((t_node*)((t_sub_elt*)function->elts[0])->elt);
+	tmp_str = get_type_str(prototype->type);
+	write(fd, tmp_str, ft_strlen(tmp_str));
+	write(fd, "\t__", 3);
+	write(fd, prototype->left->repr, ft_strlen(prototype->left->repr));
+	write(fd, "(", 1);
+	tmp_node = prototype->right;
+	while (tmp_node)
+	{
+		tmp_str = get_type_str(tmp_node->right->type);
+		write(fd, tmp_str, ft_strlen(tmp_str));
+		write(fd, " ", 1);
+		write(fd, tmp_node->right->repr, ft_strlen(tmp_node->right->repr));
+		if (tmp_node->left)
+			write(fd, ", ", 2);
+		tmp_node = tmp_node->left;
+	}
+	write(fd, ")\n{\n", 4);
+}
+
+
+
+void	write_indent_block(int fd, t_linked_list *block, int indentation, int start)
+{
+	int		i;
+
+	if (start == 0)
+	i = start;
+	while (i < block->len)
+	{
+		if (((t_sub_elt*)block->elts[i])->type == INDENT_BLOCK)
+			write_indent_block(fd, ((t_sub_elt*)block->elts[i])->elt, indentation + 1, 0);
+		else
+			write_instruction(fd, (t_node*)((t_sub_elt*)block->elts[i])->elt, indentation);
+		i++;
+	}
+}
+
+void	write_function(int fd, t_linked_list *function)
+{
+	t_node	*tmp_node;
+
+	write_function_prototype(fd, function);
+	write_indent_block(fd, function, 1, 1);
 }
 
 void	tree_to_c_code(t_linked_list *syntax_tree)
